e20ef87f64167fa860824f5411f6316c8152185a
areee <10089872+areee@users.noreply.github.com>
2021-09-18 22:47:18 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future main() async {
+  String testGh = const String.fromEnvironment('TEST_GH');
+  await dotenv.load(mergeWith: {'TEST_GH': testGh});
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(
+        brightness: Brightness.light, primaryColor: Colors.lightGreen);
+
+    final ThemeData darkTheme =
+        ThemeData(brightness: Brightness.dark, primaryColor: Colors.lightGreen);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Test dotenv in GitHub Pages',
+      theme: theme,
+      darkTheme: darkTheme,
+      home: const MyHomePage(title: 'Test dotenv in GitHub Pages'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var a = dotenv.env['VAR_NAME'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Value from dotenv file (TEST & TEST_GH keys): ',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text("'${dotenv.env['TEST']}'"),
+            Text("'${dotenv.env['TEST_GH']}'"),
+          ],
+        ),
+      ),
+    );
+  }
+}
